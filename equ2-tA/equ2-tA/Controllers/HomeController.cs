c400634fbe19e5535ae8eda053cc713bb3ca153b
Joao Correia <aluno21302@ipt.pt>
2024-03-12 20:59:03 +0000

    - Criação do controller para calcular raizes de equações de 2º grau - Criação de um controller para mostrar resultados

--- equ2-tA/equ2-tA/Controllers/HomeController.cs
+++ equ2-tA/equ2-tA/Controllers/HomeController.cs
@@ -13,9 +13,77 @@ namespace equ2_tA.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Este método é a "porta de entrada" no programa
+        /// </summary>
+        /// <param name="A">Coeficiente do parâmetro x^2</param>
+        /// <param name="B">Coeficiente do parâmetro x</param>
+        /// <param name="C">Coeficiente do parâmetro independente</param>
+        /// <returns></returns>
+        /// 
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public IActionResult Resultado(string A, string B, string C)
+        {
+            /*
+             * ALGORITMO
+             * 1º - Determinar se os parâmetros fornecidos são números
+             *  Se sim:
+             *      2º - Determinar se A =/= 0 (A<>0) (A!=0)
+             *          Se sim:
+             *              3º - Calcular as raízes:
+             *              x1,x2 = (-b +-sqrt(b^2-4*a*c))/(2*a)
+             *                  4º - Calcular as raízes reais
+             *                  5º - Calcular as raízes complexas, se existirem
+             *                  6º - Enviar resposta para o utilizador
+             *          Se não:
+             *              3º - Enviar mensagem de erro para o utilizador 
+             *  Se não:
+             *      2º - Enviar mensagem de erro ao utilizador
+             * 
+             */
+            double dA, dB, dC;
+            double x1, x2;
+
+            if (double.TryParse(A, out dA))
+            {
+                if (double.TryParse(B, out dB))
+                {
+                    if (double.TryParse(C, out dC))
+                    {
+                        if(dA != 0)
+                        {
+                            x1 = (-dB + Math.Sqrt(Math.Pow(dB, 2) - (4 * dA * dC))) / (2 * dA);
+                            x2 = (-dB - Math.Sqrt(Math.Pow(dB, 2) - (4 * dA * dC))) / (2 * dA);
+
+                            ViewBag.X1 = x1;
+                            ViewBag.X2 = x2;
+
+                            return View("Resultado");
+                        }
+                        else
+                        {
+                            return BadRequest("A tem de ser diferente de 0!");
+                        }
+                    }
+                    else
+                    {
+                        return BadRequest("C não é um número!");
+                    }
+                }
+                else
+                {
+                    return BadRequest("B não é um número!");
+                }
+            }
+            else
+            {
+                return BadRequest("A não é um número!");
+            }
         }
 
         public IActionResult Privacy()
